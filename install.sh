dbeb530733678850f8c42c3508fc5c5c95ea9ed2
Vlad Paulechka <vlad9.ru@gmail.com>
2023-01-16 13:53:33 +0300

    обнова v13

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 source ./venv/bin/activate
-/usr/bin/pip install -r /opt/git/TimeTableBot/requirements.txt
+/usr/bin/pip install -r /opt/git/TimetableBot/requirements.txt
 
-/usr/bin/python /opt/git/TimeTableBot/app.py
+/usr/bin/python /opt/git/TimetableBot/app.py
